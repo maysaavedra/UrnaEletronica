4ca1033f83ec6dedf1f3234adab5e5c827d1f5ef
maysaavedra <74421411+maysaavedra@users.noreply.github.com>
2021-08-10 18:27:29 -0300

    upload do projeto

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas=[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '13131',
+                nome:'Eduardo Suplicy',
+                partido: 'PT',
+                fotos:[
+                {url: 'suplicy.jfif', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '50700',
+                nome:'Erika Hilton',
+                partido: 'PSOL',
+                fotos:[
+                {url: 'erikaHilton.jfif', legenda: 'Vereador'}
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '50',
+                nome: 'Guilherme Boulos',
+                partido: 'PSOL',
+                vice: 'Luiza Erundina',
+                fotos:[
+                    {url: 'GuilhermeBoulos.jfif', legenda:'Prefeito'},
+                    {url: 'erundina.jfif', legenda: 'Vice-Prefeito', small: true}
+                    ]
+            },
+            {
+                numero: '45',
+                nome: 'Bruno Covas',
+                partido: 'PSDB',
+                vice: 'Ricardo Nunes',
+                fotos:[
+                    {url: 'covas.jfif', legenda:'Prefeito'},
+                    {url: 'ricardoNunes.jfif', legenda: 'vice-prefeito', small: true}
+                    ]
+            },
+            {
+                numero: '40',
+                nome: 'Márcio França',
+                partido: 'PSB',
+                vice: 'Antonio Neto',
+                fotos:[
+                    {url: 'marcioFranca.jfif', legenda:'Prefeito'},
+                    {url: 'antonioNeto.jfif', legenda: 'vice-prefeito', small: true}
+                    ]
+            },
+
+        ]
+    }
+];
+
+
+
+    
